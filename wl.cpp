aefd729a736063b31e34ea5dd8b9d824b47cd2e3
Mujahid Anuar <mujahidfanuar@gmail.com>
2019-09-07 09:41:23 -0500

    add command trimming and Enter key capture

--- wl.cpp
+++ wl.cpp
@@ -10,7 +10,34 @@
 #include <string>
 using namespace std;
 
+string &ltrim(string &str)
+{
+    const string chars = "\t\n\v\f\r ";
+    str.erase(0, str.find_first_not_of(chars));
+    return str;
+}
+
+string &rtrim(string &str)
+{
+    const string chars = "\t\n\v\f\r ";
+    str.erase(str.find_last_not_of(chars) + 1);
+    return str;
+}
+
+string &trim(string &str)
+{
+    return ltrim(rtrim(str));
+}
+
 int main()
 {
+    string command;
+    while (trim(command).compare("end") != 0)
+    {
+        cout << ">";
+        getline(cin, command); // enable Enter key capture
+        cout << "The command you entered is: " << command << endl;
+    }
+    cout << "Out of while loop";
     return 0;
 }
