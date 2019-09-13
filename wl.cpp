0fb2e269a44b26c3a029a52064142996212a8ff7
Mujahid Anuar <mujahidfanuar@gmail.com>
2019-09-12 19:25:48 -0500

    add a command parser

--- wl.cpp
+++ wl.cpp
@@ -1,10 +1,10 @@
 //
 //  File: wl.cpp
 //
-//  Description: Add stuff here ...
-//  Student Name: Add stuff here ..
-//  UW Campus ID: Add stuff here ..
-//  email: Add stuff here ..
+//  Description: Assignment 1 - CS 564 Fall 2019
+//  Student Name: Mujahid Fakhrul Anuar
+//  UW Campus ID: 9075332602
+//  email: mujahid.f.anuar@wisc.edu
 
 #include <iostream>
 #include <string>
@@ -12,11 +12,12 @@
 using namespace std;
 
 /**
- * A string utility function to remove whitespaces from a command.
+ * A utility function to remove whitespaces from a string.
  *
- * @param &str The command to be cleaned.
+ * @param str The string to be cleaned.
+ * @return A string without whitespaces.
  */
-string &removeWhitespace(string &str)
+string removeWhitespace(string str)
 {
     const string whitespaceRegex = "\r\v\n\t\f ";
     str.erase(str.find_last_not_of(whitespaceRegex) + 1); // right trim
@@ -25,71 +26,111 @@ string &removeWhitespace(string &str)
     return str;
 }
 
-string &lowercase(string &str)
+/**
+ * A utility function to convert a string to lowercase.
+ * Uses toLower().
+ *
+ * @param str The string to be lowercased.
+ * @return A lowercased string (if applicable).
+ */
+string toLowercase(string str)
 {
+    int i = 0;
+    char c;
+    while (str[i])
+    {
+        c = str[i];
+        str[i] = tolower(c);
+        i++;
+    }
     return str;
 }
 
-vector<string> tokenize(const string stringToTokenize)
+/**
+ * A helper function that returns a cleaned command.
+ *
+ * @param command The command to be cleaned.
+ * @return A vector of strings containing the cleaned commands.
+ */
+vector<string> parseCommand(string command)
 {
-    // vector<string> tokens;
-    // string token;
-    // stringstream tokenStream(s);
-    // while (getline(tokenStream, token, delimiter))
-    // {
-    //     tokens.push_back(token);
-    // }
-    // return tokens;
+    vector<string> commands;
+    command = removeWhitespace(command);
 
-    vector<string> tokens;
-
-    regex whitespaceRegex("[\r\v\n\t\f ]+");
-    sregex_token_iterator iterator(stringToTokenize.begin(), stringToTokenize.end(), whitespaceRegex, -1);
-    sregex_token_iterator regex_end;
-    while (iterator != regex_end)
+    // regex whitespaceRegex("[\r\v\n\t\f ]+");
+    regex whitespaceRegex("\\s+");
+    sregex_token_iterator itr(command.begin(), command.end(), whitespaceRegex, -1);
+    sregex_token_iterator end;
+    while (itr != end)
     {
-        tokens.push_back(iterator->str());
+        commands.push_back(toLowercase(itr->str()));
+        *itr++;
     }
 
-    return tokens;
+    return commands;
+}
+
+/**
+ * A helper function that parses a file.
+ *
+ * @param command The file name that is to be parsed.
+ * @return A vector of strings containing the parsed text.
+ */
+vector<string> parseText(string command)
+{
+    vector<string> text;
+
+    return text;
 }
 
 int main()
 {
-    string command;
-    while (removeWhitespace(command).compare("end") != 0)
+    string command; // used by cin to read commands
+    bool isLoaded = false;
+
+    do
     {
         cout << ">";
         getline(cin, command); // enable Enter key capture
 
-        // Returns first token
-        vector<string> tokenizedCommandLine = tokenize(command);
-        string firstCommand = tokenizedCommandLine[0];
+        vector<string> commands = parseCommand(command);
 
-        if (firstCommand.compare("load") == 0)
+        // debugging purposes //
+        int i = 0;
+        for (i = 0; i < commands.size(); i++)
+        {
+            cout << "Command " << i << ": |" << commands[i] << "|" << endl;
+        }
+        cout << commands[0] << endl;
+        cout << commands.size() << endl;
+        // end debugging purposes //
+
+        string firstCommand = commands[0];
+
+        if (firstCommand.compare("load") == 0 && commands.size() == 2)
         {
         }
-        else if (firstCommand.compare("locate") == 0)
+        else if (firstCommand.compare("locate") == 0 && commands.size() == 3)
         {
         }
-        else if (firstCommand.compare("new") == 0)
+        else if (firstCommand.compare("new") == 0 && commands.size() == 1)
         {
+            isLoaded = false;
         }
-        else if (firstCommand.compare("end") == 0)
+        else if (firstCommand.compare("end") == 0 && commands.size() == 1)
         {
-            // do nothing
+            command = firstCommand;
         }
         else if (firstCommand.compare("") == 0) // capture empty command
         {
-            /* code */
         }
         else // bad command
         {
             cout << "ERROR: Invalid command”" << endl;
         }
 
-        // cout << "The command you entered is: " << command << endl;
-    }
+    } while (command.compare("end") != 0);
+
     cout << "Out of while loop" << endl;
     return 0;
 }
