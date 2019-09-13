75ad72d9f339e2fa6113a02d93647b3f349ba60b
Mujahid Anuar <mujahidfanuar@gmail.com>
2019-09-13 10:20:09 -0500

    add comments

--- wl.h
+++ wl.h
@@ -1,8 +1,5 @@
+//  File: wl.h
 //
-// File: wl.h
-// 
-//  Description: Add stuff here ... 
-//  Student Name: Add stuff here .. 
-//  UW Campus ID: Add stuff here .. 
-//  email: Add stuff here .. 
-
+//  Description: Assignment #1: C++ Word Locator - CS 564 Fall 2019
+//  UW Campus ID: 9075332602
+//  email: fakhrulanuar@wisc.edu
