31001fe9e3a32cc75b34196372c1b629293fe4fe
Mujahid Anuar <mujahidfanuar@gmail.com>
2019-09-13 01:21:37 -0500

    add "load" command

--- wl.cpp
+++ wl.cpp
@@ -1,7 +1,6 @@
-//
 //  File: wl.cpp
 //
-//  Description: Assignment 1 - CS 564 Fall 2019
+//  Description: Assignment #1: C++ Word Locator - CS 564 Fall 2019
 //  Student Name: Mujahid Fakhrul Anuar
 //  UW Campus ID: 9075332602
 //  email: mujahid.f.anuar@wisc.edu
@@ -9,6 +8,8 @@
 #include <iostream>
 #include <string>
 #include <regex>
+#include <fstream> // std::ifstream
+#include <unordered_map>
 using namespace std;
 
 /**
@@ -47,7 +48,7 @@ string toLowercase(string str)
 }
 
 /**
- * A helper function that returns a cleaned command.
+ * A helper function that returns a "cleaned" command i.e. lowercased and whitespaces-removed.
  *
  * @param command The command to be cleaned.
  * @return A vector of strings containing the cleaned commands.
@@ -61,6 +62,7 @@ vector<string> parseCommand(string command)
     regex whitespaceRegex("\\s+");
     sregex_token_iterator itr(command.begin(), command.end(), whitespaceRegex, -1);
     sregex_token_iterator end;
+
     while (itr != end)
     {
         commands.push_back(toLowercase(itr->str()));
@@ -71,18 +73,11 @@ vector<string> parseCommand(string command)
 }
 
 /**
- * A helper function that parses a file.
- *
- * @param command The file name that is to be parsed.
- * @return A vector of strings containing the parsed text.
+ * Checks if a string is a valid word i.e. consists of only alphabetical characters, apostrophes, and/or numbers.
+ * 
+ * @param word The string to be checked.
+ * @return true if valid word, false otherwise
  */
-vector<string> parseText(string command)
-{
-    vector<string> text;
-
-    return text;
-}
-
 bool isValidWord(string word)
 {
     regex validWordRegex("[a-zA-Z'0-9]+");
@@ -90,39 +85,96 @@ bool isValidWord(string word)
     return regex_match(word, validWordRegex);
 }
 
-bool isValidOccurance(string word)
+/**
+ * Checks if a string is a valid number.
+ * 
+ * @param word The string to be checked.
+ * @return true if valid number, false otherwise
+ */
+bool isValidNumber(string word)
 {
-    regex validOccuranceRegex("[0-9]+");
+    regex validNumberRegex("[0-9]+");
 
-    return regex_match(word, validOccuranceRegex);
+    return regex_match(word, validNumberRegex);
 }
 
 int main()
 {
-    string command; // used by cin to read commands
+    string command; // used as placeholder by getline to read commands
     bool isLoaded = false;
+    unordered_multimap<string, int> wordList; // used to store all words and their position in the loaded file
 
     do
     {
         cout << ">";
-        getline(cin, command); // enable Enter key capture
+        getline(cin, command);
 
+        // properly parse the command, checking for whitespaces and uppercases
         vector<string> commands = parseCommand(command);
 
         // debugging purposes //
-        for (int i = 0; i < commands.size(); i++)
-        {
-            cout << "Command " << i << ": |" << commands[i] << "|" << endl;
-        }
-        cout << commands[0] << endl;
-        cout << commands.size() << endl;
+        // for (int i = 0; i < commands.size(); i++)
+        // {
+        //     cout << "Command " << i << ": |" << commands[i] << "|" << endl;
+        // }
+        // cout << commands[0] << endl;
+        // cout << commands.size() << endl;
         // end debugging purposes //
 
         string firstCommand = commands[0];
 
+        // The “load” command scans the document, & parse and store
+        // the words in a data structure i.e. wordList (unordered_multimap).
+        // It can only take 1 argument.
         if (firstCommand.compare("load") == 0 && commands.size() == 2)
         {
+            std::ifstream inputFile(commands[1], ifstream::in);
+
+            if (!inputFile)
+            {
+                cout << "Unable to open file" << endl;
+            }
+            else
+            {
+                string currLine;
+                int wordCount = 1;
+
+                while (getline(inputFile, currLine))
+                {
+                    cout << currLine << endl;
+
+                    // if line is empty, skip
+                    if (currLine.empty())
+                        continue;
+
+                    regex notValidWordRegex("[^a-zA-Z0-9']+");
+                    sregex_token_iterator itr(currLine.begin(), currLine.end(), notValidWordRegex, -1);
+                    sregex_token_iterator end;
+
+                    while (itr != end)
+                    {
+                        // add current lowercased word with the current word count as its value
+                        wordList.insert(make_pair(toLowercase(itr->str()), wordCount));
+
+                        *itr++;
+                        wordCount++;
+                    }
+                }
+
+                inputFile.close();
+            }
+
+            // print line, for debugging purposes
+            for (unordered_multimap<string, int>::iterator itr = wordList.begin(); itr != wordList.end(); itr++)
+            {
+                cout << itr->first << '\t';
+                cout << itr->second << endl;
+            }
         }
+
+        // The "locate" command, given a word, returns the position of the
+        // nth occurrence of the word in the document you load.
+        // It can only take 2 arguments.
         else if (firstCommand.compare("locate") == 0 && commands.size() == 3)
         {
             if (isValidWord(commands[1]) == true)
@@ -133,7 +185,7 @@ int main()
             {
                 cout << "ERROR: Word not valid!!" << endl;
             }
-            if (isValidOccurance(commands[2]) == true)
+            if (isValidNumber(commands[2]) == true)
             {
                 cout << "Occurance is valid" << endl;
             }
@@ -145,24 +197,35 @@ int main()
             // {
             // }
         }
+
+        // The "new" command resets the word list to original (empty) state.
+        // It should NOT take any arguments.
         else if (firstCommand.compare("new") == 0 && commands.size() == 1)
         {
-            isLoaded = false;
+            wordList.clear();
         }
+
+        // The "end" command resets the word list to its original (empty) state.
+        // It should NOT take any argument
         else if (firstCommand.compare("end") == 0 && commands.size() == 1)
         {
             command = firstCommand;
         }
-        else if (firstCommand.compare("") == 0) // capture empty command
+
+        // Captures empty commands so that it doesn't end up in the invalid commands.
+        else if (firstCommand.compare("") == 0)
         {
         }
-        else // bad command
+
+        // Handles invalid/bad commands.
+        else
         {
             cout << "ERROR: Invalid command”" << endl;
         }
 
+        command = firstCommand; // enables while loop to check for the "end" command
     } while (command.compare("end") != 0);
 
-    cout << "Out of while loop" << endl;
+    cout << "Out of while loop" << endl; // for debugging purposes
     return 0;
 }
